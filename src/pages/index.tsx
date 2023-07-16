a594f2763ff8f21b9b939639f9a39b2d5cf2b9ae
joshborseth <joshuaborseth@gmail.com>
2023-07-15 17:38:16 -0700

    hanging out and setup clerk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex h-full w-full items-start justify-end">
+      <header>
+        <UserButton />
+      </header>
+    </div>
+  );
 }
