3101217aa9721b88b63325dc38b52f9d47a62939
joshborseth <joshuaborseth@gmail.com>
2023-07-15 20:07:55 -0700

    start adding drizzle and planetscale connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex h-full w-full items-start justify-end">
+    <>
       <header>
         <UserButton />
       </header>
-    </div>
+      <div></div>
+    </>
   );
 }
