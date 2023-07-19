00a5e13ce7fe181e8af41f4d1e9727f635ba1266
joshborseth <joshuaborseth@gmail.com>
2023-07-18 18:29:38 -0700

    add in ignore eslint comment in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ export default {
   schema: "./src/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config;
